b6ca66497bc6f3c57846f89a5ab00ccd32694053
Akanksha Giliyal <108074183+AkankshaGiliyal@users.noreply.github.com>
2023-10-18 14:57:09 +0530

    Update rivera.jsx

--- rivera.jsx
+++ rivera.jsx
@@ -1,34 +1,35 @@
-const express = require('express'); // module for framework for nodejs
-const { ethers } = require('ethers');// ethereum library for interacting with smart contracts
-const MongoClient = require('mongodb').MongoClient; //mongodb driver for nodejs
+const express = require('express');
+const { ethers } = require('ethers');
+//const MongoClient = require('mongodb').MongoClient;
 
-// create an express application
+// create an Express application
 const app = express();
 const port = 3000;
+const path = require('path');
 
 // define the address and ABI of the smart contract
-const mntAddress = "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000";// ethereum address for the smart contract
-const mntABI = require('./mnt.json'); //address for the ABI
+const mntAddress = "0xfa944c1996efBF9FbFF1a378903F4AD82C172D72";
+const mntABI =require('./ABI.jsx'); // find the actual ABI file
 
-// initialize E\thereum provider and contract instances
-const provider = new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/mantle_testnet");//connects to ethereum node in mantle testnet
-const contract = new ethers.Contract(mntAddress, mntABI, provider);// smart contract instance
+// initialize Ethereum provider and contract instances
+const provider = new ethers.providers.JsonRpcProvider("https://rpc.mantle.xyz/");
+const contract = new ethers.Contract(mntAddress, mntABI, provider);
 
 // mongoDB connection details
-const mongoUrl = 'mongodb://localhost:27017'; //mongoDB connection URL
-const dbName = 'databaseNameExample';// mongoDB database name
+//const mongoUrl = 'mongodb://localhost:27017';
+//const dbName = 'defiData';
 
 // connect to MongoDB
-MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+//MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+ // if (err) {
+  //  console.error(err);
+   // return;
+ // } 
 
-  console.log('Connected to MongoDB');
+ // console.log('Connected to MongoDB');
 
   // select the database
-  const db = client.db(dbName);
+  //const db = client.db(dbName);
 
   // function to convert event data to a format suitable for MongoDB
   function formatEventData(from, to, value) {
@@ -44,42 +45,42 @@ MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true 
     const eventData = formatEventData(from, to, value);
 
     // insert the event data into MongoDB
-    const eventsCollection = db.collection('events');
-    await eventsCollection.insertOne(eventData);
+    //const eventsCollection = db.collection('events');
+    //await eventsCollection.insertOne(eventData);
 
     // print the event data to the console
     console.log(JSON.stringify(eventData, null, 4));
   });
 
   // API to fetch events data
-  app.get('/getEventsData', async (req, res) => {
-    try {
-      const eventsCollection = db.collection('events');
-      const eventsData = await eventsCollection.find().toArray();
+  //app.get('/getEventsData', async (req, res) => {
+   // try {
+    //  const eventsCollection = db.collection('events');
+    //  const eventsData = await eventsCollection.find().toArray();
 
-      res.json({ events: eventsData });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+     // res.json({ events: eventsData });
+   // } catch (error) {
+   //   console.error(error);
+   //   res.status(500).json({ error: 'Internal Server Error' });
+  //  }
+  //});
 
   // API to fetch function data
-  app.get('/getFunctionData', async (req, res) => {
-    try {
-     
-      const decimal = await contract.decimals();
+  //app.get('/getFunctionData', async (req, res) => {
+  //  try {
+      // Example: Call a function on the smart contract
+    //  const decimal = await contract.decimals();
 
-      
-      res.json({ decimal });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+      // Store the result in MongoDB or send it as a response
+    //  res.json({ decimal });
+  //  } catch (error) {
+   //   console.error(error);
+   //   res.status(500).json({ error: 'Internal Server Error' });
+  //  }
+  //});
 
-  // start the express server
+  // start the Express server
   app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
   });
-});
+//});
